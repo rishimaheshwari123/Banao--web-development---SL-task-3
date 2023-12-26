c938e39c30b7047e23833ffe9a18aef4931d5688
Rishi <rishimaheshwari040@gmail.com>
2023-12-26 12:49:37 +0530

    update

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
     const submitBtn = document.querySelector(".btn-primary");
+    const inputField = document.querySelector(".input-large");
 
     submitBtn.addEventListener("click", function () {
         submitBtn.disabled = true;
         submitBtn.classList.add("loading");
 
+        inputField.disabled = true;
+
         setTimeout(function () {
             submitBtn.disabled = false;
             submitBtn.classList.remove("loading");
         }, 2000);
+
+        setTimeout(function () {
+            inputField.disabled = false;
+        }, 3000000);
     });
 });
